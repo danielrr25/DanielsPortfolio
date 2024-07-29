834ff3cedf81358ee20c9f70553a85300eead231
danielrr25 <danielrr25@hotmail.com>
2024-07-29 16:05:21 -0400

    almost ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/dr3.ico" sizes="any" />
+        <link rel="icon" href="/dr3.ico" sizes="any" type="image/x-icon" />
+        <link rel="shortcut icon" href="/dr3.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/dr3.ico" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
