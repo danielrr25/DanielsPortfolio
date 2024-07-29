93ec8a9867adf286db3a2bba719b82bfce9f7dbc
danielrr25 <danielrr25@hotmail.com>
2024-07-29 13:13:47 -0400

    setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,56 @@
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-// import { ThemeProvider } from "next-themes";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
-// const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Daniel Rodriguez",
-//   description: "Welcome to my Portfolio",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en" suppressHydrationWarning>
-//       <head>
-//         <link rel="icon" href="/dr3.ico" sizes="any" />
-//       </head>
-//       <body className={inter.className}>
-//         <ThemeProvider
-//           attribute="class"
-//           defaultTheme="dark"
-//           enableSystem
-//           disableTransitionOnChange
-//         >
-//           {children}
-//         </ThemeProvider>
-//       </body>
-//     </html>
-//   );
-// }
-
-import './globals.css';
-import { ReactNode } from 'react';
-
-export const metadata = {
-  title: "Daniel's Portfolio",
-  description: 'Welcome to my Portfolio',
+export const metadata: Metadata = {
+  title: "Daniel Rodriguez",
+  description: "Welcome to my Portfolio",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/dr3.png" sizes="any" />
+        <link rel="icon" href="/dr3.ico" sizes="any" />
       </head>
-      <body>
-        {children}
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
 }
+
+// import './globals.css';
+// import { ReactNode } from 'react';
+
+// export const metadata = {
+//   title: "Daniel's Portfolio",
+//   description: 'Welcome to my Portfolio',
+// };
+
+// export default function RootLayout({ children }: { children: ReactNode }) {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <link rel="icon" href="/dr3.png" sizes="any" />
+//       </head>
+//       <body>
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,12 @@
+// import { FaLocationArrow } from "react-icons/fa6";
+
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
-      {/* UI: Spotlights */}
+    <div className="pb-20 pt-36 relative">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -18,12 +19,10 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* UI: grid */}
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
       >
-        {/* Radial gradient for the container to give a faded look */}
         <div
           className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
@@ -33,32 +32,29 @@ const Hero = () => {
       <div className="flex justify-center relative my-5 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <div className="flex flex-row items-center mb-10">
-            <h1 className="uppercase tracking-widest text-5xl text-center mr-2 text-[#a7b3ff]">
+            <h1 className="uppercase tracking-widest text-5xl text-center mr-2 text-custom-color">
               Daniel
             </h1>
-            <h1 className="uppercase tracking-widest text-5xl text-center text-[#a7b3ff]">
+            <h1 className="uppercase tracking-widest text-5xl text-center text-custom-color">
               Rodriguez
             </h1>
           </div>
-          
-          {/* Subheading Section */}
+
           <div className="mb-10">
             <TextGenerateEffect
               words="Transforming Concepts into Seamless User Experiences"
-              className="text-center text-[40px] md:text-4xl lg:text-5xl"
+              className="text-center text-[40px] md:text-4xl lg:text-5xl text-custom-color"
             />
           </div>
 
-          {/* Additional Message */}
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-custom-color">
             A recent computer science graduate student with experience in full-stack web development. My passion for programming is driven by a curiosity to solve real-world challenges and innovate with new ideas and websites.
           </p>
-          
-          {/* Button Section */}
+
           <a href="#about">
             <MagicButton
               title="Show my work"
-              icon="→"
+              icon="->"
               position="right"
             />
           </a>
