2d6d36a9c44f8d472bd74769d195c5e9a84f9478
danielrr25 <danielrr25@hotmail.com>
2024-07-29 14:45:15 -0400

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,3 @@
-// "use client";
-
-// // import { navItems } from "@/data";
-
-// // import Hero from "@/components/Hero";
-// // import Grid from "@/components/Grid";
-// // import Footer from "@/components/Footer";
-// // // import Clients from "@/components/Clients";
-// // // import Approach from "@/components/Approach";
-// // import Experience from "@/components/Experience";
-// // import RecentProjects from "@/components/ui/RecentProjects";
-// // import { FloatingNav } from "@/components/ui/FloatingNav";
-
-// const Home = () => {
-//   return (
-//     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-//       <div className="max-w-7xl w-full">
-//         {/* <FloatingNav navItems={navItems} />
-//         <Hero />
-//         <Grid />
-//         <RecentProjects />
-//         <Experience />
-//         <Footer />  */}
-//         {/*
-//         <Clients />
-        
-//         <Approach />
-//         */}
-//       </div>
-//     </main>
-//   );
-// };
-
-// export default Home;
 
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
@@ -40,6 +6,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
@@ -47,9 +14,10 @@ export default function Home() {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <About />
+        <Grid />
+        {/* <About />
         <Projects />
-        <Contact />
+        <Contact /> */}
       </div>
     </main>
   );
