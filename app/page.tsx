cb063259a419d1578a363b40a7b7458e944bf7b0
danielrr25 <danielrr25@hotmail.com>
2024-07-29 11:48:08 -0400

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -34,20 +34,20 @@
 // export default Home;
 
 
-
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
 
-export default function HomePage() {
+export default function Home() {
   return (
-    <>
-      <Hero />
-      <About />
-      <Projects />
-      <Contact />
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
+    </main>
   );
 }
-
