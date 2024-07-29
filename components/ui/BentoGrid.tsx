830802fbfb1d15028bc125897b09b4208be322db
danielrr25 <danielrr25@hotmail.com>
2024-07-29 16:48:22 -0400

    almost ready

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -60,7 +60,12 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1]",
+        "relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.1]",
+        {
+          'md:col-span-3 md:row-span-1': id === 1, // First item spans 3 columns in the first row
+          'md:col-span-3 md:row-span-1': id === 2, // Second item spans 3 columns in the first row
+          'md:col-span-3 md:row-span-1': id === 3, // Third item spans 3 columns in the second row
+        },
         className
       )}
       style={{
@@ -91,9 +96,7 @@ export const BentoGridItem = ({
           )}
         </div>
         {id === 6 && (
-          // add background animation , remove the p tag
           <BackgroundGradientAnimation>
-            {/* <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div> */}
           </BackgroundGradientAnimation>
         )}
 
@@ -103,20 +106,15 @@ export const BentoGridItem = ({
             "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
         >
-          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
           <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 mt-[-20px]">            
             {description}
           </div>
-          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-          {/* remove mb-2 mt-2 */}
           <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>
             {title}
           </div>
 
-          {/* for the github 3d globe */}
           {id === 2 && <GridGlobe />}
 
-          {/* Tech stack list div */}
           {id === 3 && (
             <div className="grid grid-cols-2 gap-2">
               {leftLists.concat(rightLists).map((item, i) => (
@@ -146,5 +144,3 @@ export const BentoGridItem = ({
     </div>
   );
 };
-
-
