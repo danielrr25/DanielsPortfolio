4c2e330910554e897a6afc51f4e05632fe355a19
danielrr25 <danielrr25@hotmail.com>
2024-10-26 20:12:17 -0400

    Add my new portfolio project

--- components/ui/RecentProjects.tsx
+++ components/ui/RecentProjects.tsx
@@ -52,7 +52,7 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Please check back later
+                    
                   </p>
                   {/* <FaLocationArrow className="ms-3" color="#CBACF9" /> */}
                 </div>
